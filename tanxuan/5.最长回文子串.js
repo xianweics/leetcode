6406dc2321377322b42d1aba1d7d877cfc3e0e86
tanxuan <tanxuan@daishujiankang.com>
2019-10-25 17:55:51 +0800

    note: 第5,7,8,9题，题目：提交；提交信息：提交

---
+++ tanxuan/5.最长回文子串.js
@@ -0,0 +1,48 @@
+/*
+ * @lc app=leetcode.cn id=5 lang=javascript
+ *
+ * [5] 最长回文子串
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var longestPalindrome = function(s) {
+  let len = s.length - 1,
+    arr = [],
+    target = '',
+    curr = ''
+  if (len === 0) {
+    return s
+  }
+  if ([...new Set(s)].length === 1) {
+    return s
+  }
+  for (let i = 0; i <= len; i++) {
+    arr[i] = []
+    for (let j = 0; j <= len; j++) {
+      arr[i][j] = i === j ? true : false
+    }
+  }
+  for (let i = len; i >= 0; i--) {
+    for (let j = len - 1; j >= 0; j--) {
+      if (i - 1 < 0) {
+        continue
+      }
+      if (s[i - 1] === s[j + 1]) {
+        arr[i - 1][j + 1] = arr[i][i]
+      } else {
+        arr[i - 1][j + 1] = false
+      }
+    }
+    let start = 0, end = 0
+    start = arr[i].indexOf(true), end = arr[i].lastIndexOf(true)
+    curr = s.slice(start, end+1)
+    target = target.length >= curr.length ? target : curr
+  }
+  console.log(arr, target)
+  return target
+}
+// @lc code=end
