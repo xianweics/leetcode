d96850bdcedbf88e7234a853086964e9a8253fe4
tanxuan <tanxuan@daishujiankang.com>
2019-10-29 15:10:21 +0800

    update: 第5题，题目：最长回文子串；提交信息：更新

--- tanxuan/5.最长回文子串.js
+++ tanxuan/5.最长回文子串.js
@@ -10,62 +10,73 @@
  * @return {string}
  */
 var longestPalindrome = function(s) {
-  /* let len = s.length - 1,
+  // dp
+  /* let len = s.length,
     arr = [],
-    target = '',
-    curr = ''
-  if (len === 0) {
+    start = 0,
+    max = 0
+  // 1. 先过滤掉特殊长度
+  if (len <= 1 || [...new Set(s)].length === 1) {
     return s
   }
-  if ([...new Set(s)].length === 1) {
-    return s
-  }
-  for (let i = 0; i <= len; i++) {
-    arr[i] = []
-    for (let j = 0; j <= len; j++) {
-      arr[i][j] = i === j ? true : false
+  // 2. 初始化并找到是否长度为2的回文串
+  for (let i = 0; i < len; i++) {
+    arr[i] = new Array(len)
+    arr[i].fill(false)
+    arr[i][i] = true
+    if (i < len - 1 && s[i] === s[i + 1]) {
+      arr[i][i + 1] = true
+      max = 2
+      start = i
     }
   }
-  for (let i = len; i >= 0; i--) {
-    for (let j = len - 1; j >= 0; j--) {
-      if (i - 1 < 0) {
-        continue
-      }
-      if (s[i - 1] === s[j + 1]) {
-        arr[i - 1][j + 1] = arr[i][i]
-        // curr = j - i + 2
-        // if (curr > target.length) {
-        //   target = s.slice(Math.min(i,j), Math.max(i,j)+1)
-        // }
-      } else {
-        arr[i - 1][j + 1] = false
-      }
-    }
-    let start = 0, end = 0
-    start = arr[i].indexOf(true), end = arr[i].lastIndexOf(true)
-    curr = s.slice(start, end+1)
-    target = target.length >= curr.length ? target : curr
-  }
-  console.log(arr, target)
-  return target */
-
-  let len = s.length, target = '', arr = []
-
-  if (s.length === 1 || [...new Set(s)].length === 1) {
-    return s
-  }
-
-  for(let i = 0; i < len; i ++) {
-    arr[i] = []
-    for(let j = 0; j < len; j ++) {
-      let curr = s.slice(i, j + 1)
-      let str = curr.split('').reverse().join('')
-      if (curr == str) {
-        target = target.length >= curr.length ? target : curr
+  // 3. 从长度3开始遍历整个串
+  for (let l = 3; l <= len; l++) {
+    // 4. 根据前一个状态判断当前串是否是回文,并更新状态
+    for (let i = 0; i + l - 1 < len; i++) {
+      let j = i + l - 1
+      if (s[i] === s[j] && arr[i + 1][j - 1]) {
+        arr[i][j] = true
+        start = i
+        max = l
       }
     }
   }
-  return target
+  // 5. 如果不存在回文则返回第一个字符
+  if (max === 0 && len > 1) {
+    return s[0]
+  }
+  return s.substr(start, max) */
+
+  // 中心扩散
+  let len = s.length
+  if (len <= 1 || [...new Set(s)].length === 1) {
+    return s
+  }
+  let start = 0, // 起始索引
+    end = 0, // 结束索引
+    max = 0 // 最大长度
+  for (let i = 0; i < len; i++) {
+    let len1 = calcCenter(s, i, i) // 一个元素为中心
+    let len2 = calcCenter(s, i, i + 1) // 两个元素为中心
+    max = Math.max(Math.max(len1, len2), max) // 计算当前的最大长度
+    // 根据长度计算起止索引
+    if (max > end - start + 1) {
+      start = i - Math.floor((max - 1) / 2)
+      end = i + Math.floor(max / 2)
+    }
+  }
+  return s.substr(start, max)
+}
+
+function calcCenter(string, left, right) {
+  // 从当前元素为中心 向两边扩散查找,直到找到不同的两个元素
+  while (left >= 0 && right < string.length && string[left] === string[right]) {
+    left--
+    right++
+  }
+  // 因为向两边扩散是同时进行的,且不成立才停止所以长度由 r - l + 1 => r - l -1
+  return right - left - 1
 }
 
 // @lc code=end
