435d94e92011cf7fd1cb652c912f76054ba5b804
SJH <1039460820@qq.com>
2019-11-17 23:01:34 +0800

    两输相加

--- shenjunhong/twoSub#02.js
+++ shenjunhong/twoSub#02.js
@@ -14,6 +14,55 @@
  * @return {ListNode}
  */
 var addTwoNumbers = function(l1, l2) {
-    l1
+    function LList(){
+      this.head = new Node('Head'); //头节点
+      this.find = find; //查找节点
+      this.insert = insert; //插入节点
+      this.remove = remove; //移除节点
+      this.findre = findPrev; //查找前一个节点
+      this.display = display;  //显示链表
+    }
+    function find(item) {
+      var curNode = this.head;
+      while(curNode.element != item){
+        curNode = curNode.next;
+      }
+      return curNode;
+    }
+
+
+    var c1 = l1,
+    c2 = l2,
+    l3, c3,
+    carry = 0;
+
+  while (c1 || c2 || carry) {
+    var v1 = 0,
+      v2 = 0;
+    
+    // 这么做是为了防止整数中当前位上没有数字
+    if (c1) {
+      v1 = c1.val;
+      c1 = c1.next;
+    }
+
+    if (c2) {
+      v2 = c2.val;
+      c2 = c2.next;
+    }
+
+    var sum = v1 + v2 + carry;
+    carry = Math.floor(sum / 10);
+
+    if (!c3) {
+      l3 = new ListNode(sum % 10);
+      c3 = l3;
+    } else {
+      c3.next = new ListNode(sum % 10);
+      c3 = c3.next;
+    }
+
+  }
+  return l3;
 };
 
