fbd2854e14f1f682e312c6a387f013d22460085d
SJH <1039460820@qq.com>
2019-12-15 22:43:10 +0800

    增加了两道题目

--- shenjunhong/移除元素#27.js
+++ shenjunhong/移除元素#27.js
@@ -3,24 +3,40 @@
  * @param {number} val
  * @return {number}
  */
+// var removeElement = function(nums, val) {
+//   // console.log('nums, val: ', nums, val);
+//   var arr = [];
+//   arr = [...nums]
+//   for(let i in nums){
+//     console.log('nums: ', nums);
+//     if(nums[i] == val){
+//       // console.log('i: ', nums[i]), i;
+//       arr.slice(i,1)
+//     }
+//   }
+//   return nums = arr;
+// };
+
+
 var removeElement = function(nums, val) {
-  var arr = [];
-  arr = nums
-  for(let i in nums){
-    console.log('i: ', i);
-    if(nums[i] == val){
-      arr.splice(i,1)
-    }
-  }
-  return nums = arr;
+  let len = nums.length;
+  let s = 0;
+  for(let i=0;i<len;i++)
+      {
+          if(nums[i] != val)
+              {
+                  nums[s++] = nums[i];
+              }
+      }
+  return s;
 };
 
 // var val = removeElement([2,3,4,5,3,6], 5)
-var val = removeElement([3,2,2,3],3)
+// var val = removeElement([3,2,2,3],3)
+var val = removeElement([0,1,2,2,3,0,4,2],2)  //01304
+
 
 console.log('val: ', val);
-
-
 
 
 
