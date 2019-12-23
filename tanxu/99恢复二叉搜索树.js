45a7588ada59bc4615a046bdc82f93afbbfa4013
6F6B1E2A03AC8CE538B8751D48ED4198 <6F6B1E2A03AC8CE538B8751D48ED4198@i-search.com.cn>
2019-12-23 11:09:42 +0800

    新增 第九周进度截图与代码笔记

--- tanxu/99恢复二叉搜索树.js
+++ tanxu/99恢复二叉搜索树.js
@@ -15,15 +15,13 @@ var recoverTree = function(root) {
     let node1 = null
     let node2 = null
     while(curNode !== null) {
-        if (node1 && node2) break
-        if (preNode && curNode.val <= preNode.val) {
-            if (!node1) {
-                node1 = preNode
-            } else {
+        if (curNode.left === null) {
+            // 这里记录下错误的节点
+            if (preNode && preNode.val >= curNode.val) {
+                node1 = node1 === null ? preNode : node1
                 node2 = curNode
             }
-        }
-        if (curNode.left === null) {
+            // console.log(preNode ? preNode.val : null, curNode.val)
             preNode = curNode
             curNode = curNode.right
         } else {
@@ -33,18 +31,43 @@ var recoverTree = function(root) {
                 node = node.right
             }
             if (node.right === curNode) {
+                // 这里记录下错误的节点
+                if (preNode && preNode.val >= curNode.val) {
+                    node1 = node1 === null ? preNode : node1
+                    node2 = curNode
+                }
+               // console.log(preNode ? preNode.val : null, curNode.val)
+                preNode = curNode
                 curNode = curNode.right
                 node.right = null
             } else {
                 // 设置线索
                 node.right = curNode
-                preNode = curNode
                 // 继续往左遍历
                 curNode = curNode.left
             }
         }
     }
-    let val = node1.val
+    // 交换值
+    let temp = node1.val
     node1.val = node2.val
-    node2.val = val
+    node2.val = temp
 };
+recoverTree(createTree())
+
+
+
+function TreeNode(val, left, right) {
+    this.val = val;
+    this.left = left
+    this.right = right;
+}
+
+function createTree () {
+    let node3 = new TreeNode(3, null, null)
+    let node7 = new TreeNode(7, null, null)
+    let node10 = new TreeNode(10, null, null)
+    let node8 = new TreeNode(8, node3, node10)
+    let root = new TreeNode(6, node7, node8)
+    return root
+}
