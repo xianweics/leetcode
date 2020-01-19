2c2bd318122cd7cddd4545b89e8c53649e20272a
SJH <1039460820@qq.com>
2020-01-19 22:15:08 +0800

    提交了三道题#01

---
+++ shenjunhong/二叉树的最大深度#104.js
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function(root) {
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+};
