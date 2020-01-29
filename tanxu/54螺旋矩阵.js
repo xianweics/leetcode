60e11ea99b86df2906d315ee9ae7ca55bf09a1e2
tanxu1a <269828350@qq.com>
2020-01-29 13:54:09 +0800

    提交第54题代码

--- tanxu/54螺旋矩阵.js
+++ tanxu/54螺旋矩阵.js
@@ -3,6 +3,43 @@
  * @return {number[]}
  */
 var spiralOrder = function(matrix) {
-    let i = matrix.length, j = matrix[0].length
+    let record = []
+    for (let i = 0; i < matrix.length; i++) {
+        record[i] = []
+        for (let j = 0; j < matrix[i].length; j++) {
+            record[i][j] = true
+        }
+    }
+    record[matrix.length] = []
+    let res = []
+    let dir = 'right'
+    let i = 0
+    let j = 0
+    while (record[i][j]) {
+        console.log(i,j, dir)
+        res.push(matrix[i][j])
+        record[i][j] = false
+        switch (dir) {
+            case "right": j++;break;
+            case "down": i++; break;
+            case "left": j--; break;
+            case "top": i--; break;
+        }
+
+
+        if (!record[i][j]) {
+            switch (dir) {
+                case "right": dir = "down";j--;i++;break;
+                case "down": dir = "left";i--;j--; break;
+                case "left": dir = "top";j++;i--; break;
+                case "top": dir = "right";i++;j++; break;
+            }
+        }
+    }
+
+    return res
 
 };
+
+
+console.log(spiralOrder([[1,2,3], [4,5,6], [7,8,9]]))
